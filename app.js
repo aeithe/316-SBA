7b0f78adfbb1195984590c9f4cebb1d332a1ad37
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-30 12:35:01 -0400

    did some stuff

---
+++ app.js
@@ -0,0 +1,6 @@
+const image = document.getElementById("img");
+// use for child/parent relationship VV
+//const mainEl = document.querySelector("main");
+
+const gallery = document.querySelector("#gallery");
+gallery.style
