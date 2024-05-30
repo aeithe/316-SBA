207466bef34ed051ae053f52bbec3d9055b2baad
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-30 12:35:26 -0400

    fixed some other stuff

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const image = document.getElementById("img");
-// use for child/parent relationship VV
-//const mainEl = document.querySelector("main");
 
-const gallery = document.querySelector("#gallery");
-gallery.style
+const mainEl = document.querySelector("main");
+mainEl.style.background = "var(--main-bg)";
+
+const navBar = document.createElement("nav");
+navBar.style.height = "50%";
+navBar.style.backgroundColor = "var(--nav-bg)"
+navBar.classList.add = ("flex-ctr");
+mainEl.append(navBar);
+
+// console.log(navBar, menuLinks)
+
+const menuLinks = [
+    {text: 'home', link: '/index.html'},
+    {text: 'contact', link: '/contact_index.html'}
+];
+console.log(navBar, mainEl);
+navBar.forEach((link) =>{
+    const aElement = document.createElement("a");
+    aElement.link = link.link;
+    aElement.textContent = link.text; 
+    navBar.appendChild(aElement);
+
+});
